a8718197ee2062dfdd8096b4055c9be01eb9d91c
William Whitley <innocent.oreo@gmail.com>
2022-07-29 07:44:42 +0100

    feat: parsing email to pdf

--- SainsburysDownloader.ts
+++ SainsburysDownloader.ts
@@ -1,5 +1,7 @@
 import Imap from "node-imap";
 import fs from "fs";
+import path from "path";
+import Base64 from "js-base64";
 
 function openInboxPromise(imap: Imap) {
   return new Promise<Imap.Box>((resolve, reject) =>
@@ -33,6 +35,17 @@ function fetchEndPromise(fetch: Imap.ImapFetch) {
   });
 }
 
+// taken from:
+// https://stackoverflow.com/questions/10623798/how-do-i-read-the-contents-of-a-node-js-stream-into-a-string-variable
+function streamToString(stream: NodeJS.ReadableStream) {
+  const chunks: Buffer[] = [];
+  return new Promise<string>((resolve, reject) => {
+    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on("error", (err) => reject(err));
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+  });
+}
+
 function searchPromise(imap: Imap, searchParams: any[]) {
   return new Promise<number[]>((resolve, reject) =>
     imap.seq.search(searchParams, (err, ids) => {
@@ -56,9 +69,65 @@ class SainsburysDownloader {
 
   imapPassword: string;
 
+  destinationFolder: string;
+
   constructor() {
     this.imapUser = "";
     this.imapPassword = "";
+    this.destinationFolder = "";
+  }
+
+  async handleStream(stream: NodeJS.ReadableStream) {
+    // get the stream into a string for easyparsing
+    const msg = await streamToString(stream);
+
+    // find the bit in the string where the PDF begins
+    // it begins with info on the file name as well
+    const startRegExp = new RegExp(
+      /Content-Type: application\/pdf\; name=\"sainsburys_groceries_order_[0-9]+.pdf\"/g
+    );
+    const startMatchVal = startRegExp.exec(msg);
+
+    if (startMatchVal === null) {
+      console.log("couldn't find PDF content in the email");
+      return;
+    }
+
+    // extract the PDF file name from the email content
+    let fileName = startMatchVal[0].split(`"`)[1];
+    if (this.destinationFolder !== "") {
+      fileName = path.join(this.destinationFolder, fileName);
+    }
+
+    // now move to get the PDF content alone
+    // start by finding the bit of the email with the PDF content
+    // plus a bit extra at the end
+    const stringWithExtra = msg.slice(
+      startMatchVal.index + startMatchVal[0].length
+    );
+
+    // the PDF content ends with a string bracketed by hyphens
+    const endRegExp = new RegExp(/--[A-z0-9]+--/g);
+    const endMatchVal = endRegExp.exec(stringWithExtra);
+
+    if (endMatchVal === null) {
+      console.log("couldn't isolate PDF content in the email");
+      return;
+    }
+
+    // extract PDF content and trim off whitespace from star / end
+    const finalString = stringWithExtra.slice(0, endMatchVal.index).trim();
+
+    // convert content from base64 to binary, then write to file
+    // with help from https://stackoverflow.com/questions/56483097/decoding-base64-pdf-giving-broken-file
+    var bin = Base64.atob(finalString);
+    fs.writeFile(fileName, bin, "binary", (error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log("binary saved!");
+      }
+    });
   }
 
   async download() {
@@ -70,7 +139,7 @@ class SainsburysDownloader {
       tls: this.imapTls,
     });
 
-    console.log("wait");
+    console.log("Connecting to server...");
 
     const ready = imapReadyPromise(imap);
 
@@ -78,13 +147,17 @@ class SainsburysDownloader {
 
     await ready;
 
+    console.log("Connected! Opening inbox...");
+
     const box = await openInboxPromise(imap);
+
+    console.log("Opened! Searching for Sainsbury's emails...");
 
     const ids = await searchPromise(imap, [
       ["FROM", "service@sainsburys.co.uk"],
     ]);
 
-    console.log(ids);
+    console.log(`Found ${ids.length} email(s). Now getting headers...`);
 
     var f = imap.seq.fetch(ids, {
       bodies: "HEADER.FIELDS (FROM TO SUBJECT DATE)",
@@ -98,12 +171,19 @@ class SainsburysDownloader {
         const struct = attrs.struct as [{ subtype?: string }][];
         const pdfPart = struct.find((el) => el[0] && el[0].subtype === "pdf");
         if (pdfPart) {
+          console.log(`PDF found in message #${seqno}`);
           seqNosWithPdfs.push(seqno);
+        } else {
+          console.log(`No PDF in message #${seqno}`);
         }
       });
     });
 
     await fetchEndPromise(f);
+
+    console.log(
+      "Finished getting headers. Now getting body text for messages with PDFs..."
+    );
 
     console.log(seqNosWithPdfs);
 
@@ -112,11 +192,12 @@ class SainsburysDownloader {
       struct: true,
     });
 
-    f.on("message", function (msg, seqno) {
-      console.log("Message #%d", seqno);
+    f.on("message", (msg, seqno) => {
+      console.log("Getting body for message #%d", seqno);
       var prefix = "(#" + seqno + ") ";
-      msg.on("body", function (stream, info) {
-        stream.pipe(fs.createWriteStream("msg-" + seqno + "-body.txt"));
+      msg.on("body", (stream, info) => {
+        console.log("Handling stream for message #%d", seqno);
+        this.handleStream(stream);
       });
     });
 
